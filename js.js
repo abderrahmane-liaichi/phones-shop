0e7f426836cb5abe1ff4c59095fc36d172ef618b
Abderrahmane Liaichi <userliaichiabderrahmane@gmail.com>
2025-05-03 18:28:42 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img");
+let contiener = document.querySelector('.contiener');
+
+
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    contiener.style.background = color;
+}
